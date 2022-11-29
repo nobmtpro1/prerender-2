644d97a77eae648295587764dea345d7041992a2
Nguyễn Bá Thái <thai.nguyen@aegona.com>
2022-11-29 13:31:49 +0700

    up

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+
+const path = require("path");
+
+// app.use(
+//     require("prerender-node").set(
+//         "prerenderServiceUrl",
+//         "http://localhost:3000"
+//     )
+// );
+
+app.use(require('prerender-node').set('prerenderToken', 'd8rQcPll8oyKTEdQs2xZ'));
+
+app.use("/", express.static(path.join(__dirname, "reactjs/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "reactjs/build", "index.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
